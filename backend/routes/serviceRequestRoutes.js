f72b339995e1fed0464165f35870e1c704b8fb6e
dasunithiwanshika-ship-it <dasunithiwanshikagmail.com>
2026-04-01 20:09:09 +0530

    Add backend/routes/serviceRequestRoutes.js

---
+++ backend/routes/serviceRequestRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
+const {
+    createServiceRequest,
+    getAllServiceRequests,
+    getUserServiceRequests,
+    getServiceRequestById,
+    updateServiceRequestStatus,
+    uploadProgressImage
+} = require('../controllers/serviceRequestController');
+
+// All routes here will be protected by authMiddleware
+router.use(authMiddleware);
+
+// Routes
+router.post('/', createServiceRequest);
+router.get('/', getAllServiceRequests);
+router.get('/my-requests', getUserServiceRequests);
+router.get('/:id', getServiceRequestById);
+router.put('/:id/status', updateServiceRequestStatus);
+router.put('/:id/progress-image', uploadProgressImage);
+
+module.exports = router;
